3a554166228f9d9d9dbd61d6e0bbd5554d614870
Enzup <enzupgarg@gmail.com>
2024-02-02 14:11:51 +0530

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Accordion from "./components/ui/Accordion";
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion text="Some Text" content={"asdf"}></Accordion>
+    </div>
+  );
+}
+
+export default App;
